5f9f5c5b86e31a9bf4bea9a8ccfb84e11831abd4
marcelo <sjsjsjmarcelo@gmail.com>
2024-02-07 17:34:18 -0300

    Function Save Task complet

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -6,12 +6,32 @@ import { MdDeleteForever } from "react-icons/md";
 import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
 
 export default function Task({ note, onDelete }) {
-    const [completed, setCompleted] = useState(false)
+    const [completed, setCompleted] = useState(note.complet)
+
+    function handleComplet() {
+        setCompleted(!completed)
+    }
+
+    useEffect(() => {
+        const tasks = JSON.parse(localStorage.getItem("TAREFA_EXPRESS:tasks"))
+
+        const filterTasks = tasks.filter(task => task.id !== note.id)
+        const taskSelected = tasks.filter(task => task.id === note.id)
+
+        taskSelected[0].complet = completed
+
+        localStorage.setItem("TAREFA_EXPRESS:tasks", JSON.stringify([...filterTasks, ...taskSelected]))
+
+        if (taskSelected[0].complet === true) {
+            setCompleted(true)
+        }
+
+    }, [completed])
 
     return (
         <div className={`card__task  ${completed && 'completed'}`}>
             <div className="complet__wrapper">
-                <button id='btn-complet' onClick={() => setCompleted(!completed)}>
+                <button id='btn-complet' onClick={handleComplet}>
                     {completed ? <FaRegCheckCircle /> : <FaRegCircle />}
                 </button>
                 <span>{note.name}</span>
